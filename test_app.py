af50d881d66948a912e837f6f9ca0fa8c096326e
dmishra-ymsli <dmishra@yamaha-motor-india.com>
2025-04-15 22:26:15 +0530

    pull request testing

--- test_app.py
+++ test_app.py
@@ -9,4 +9,4 @@ def client():
 def test_home_route(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b"Flask API is running!"
+    assert response.data == b"Flask API was running!"
